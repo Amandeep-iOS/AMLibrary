6e368456d030149edcf2a7cee803328668efad2b
Amandeep Singh <amandps86@gmail.com>
2016-07-27 12:08:36 +0530

    Initial Commit

---
+++ Example/Pods/Target Support Files/AMLibrary/AMLibrary-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_AMLibrary : NSObject
+@end
+@implementation PodsDummy_AMLibrary
+@end
---
+++ Example/Pods/Target Support Files/AMLibrary/AMLibrary-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double AMLibraryVersionNumber;
+FOUNDATION_EXPORT const unsigned char AMLibraryVersionString[];
+
